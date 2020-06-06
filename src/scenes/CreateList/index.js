bdc77ab5e55f3c4ebf7dd64af819caac81fd6819
Isma <didouhismael@gmail.com>
2020-06-06 16:58:33 +0200

    create list component

---
+++ src/scenes/CreateList/index.js
@@ -0,0 +1,62 @@
+// libs
+import React, { useState } from "react";
+
+// services
+import * as FirestoreService from "../../services/firestore";
+
+// styles
+import "./index.css";
+
+// error handlers
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+
+const CreateList = (props) => {
+  const { onCreate, userId } = props;
+
+  const [error, setError] = useState();
+
+  function createGroceryList(e) {
+    e.preventDefault();
+    setError(null);
+
+    const userName = document.createListForm.userName.value;
+    if (!userName) {
+      setError("user-name-required");
+      return;
+    }
+
+    FirestoreService.createGroceryList(userName, userId)
+      .then((docRef) => {
+        onCreate(docRef.id, userName);
+      })
+      .catch((reason) => setError("create-list-error"));
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>Welcome to the Grocery List app!</h1>
+      </header>
+      <div className="create-container">
+        <div>
+          <form name="createListForm">
+            <p>
+              <label>What is your name?</label>
+            </p>
+            <p>
+              <input type="text" name="userName" />
+            </p>
+            <ErrorMessage errorCode={error}></ErrorMessage>
+            <p>
+              <button onClick={createGroceryList}>
+                Create a new grocery list
+              </button>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateList;

faeee4272b58fa0a9d3651fd2e7c5a8966043d6f
Isma <didouhismael@gmail.com>
2020-06-06 14:23:24 +0200

    install firebase + create grocery list method

--- src/services/firestore/index.js
+++ src/services/firestore/index.js
@@ -2,11 +2,22 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+// config
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
+// init
 firebase.initializeApp(firebaseConfig);
+
+// shorthands
 const db = firebase.firestore();
+
+export const createGroceryList = (userName) => {
+  return db.collection("groceryLists").add({
+    created: firebase.firestore.FieldValue.serverTimestamp(),
+    users: [{ name: userName }],
+  });
+};
